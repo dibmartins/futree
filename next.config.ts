d17ae2ab8b7a31131d5fc9e2049a428627d4cfa0
Diego Botelho <dibmartins@gmail.com>
2026-05-04 20:52:44 -0300

    feat: ajuste lib remove bg

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["@imgly/background-removal-node", "onnxruntime-node"],
   images: {
     remotePatterns: [
       {
